a43899dc070c71b5971f45cbb413375bacd03db9
Sarah Lee <sarahselee223@gmail.com>
2022-06-19 15:14:02 -0700

    Initial Commit

---
+++ CovidList/CovidListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidListApp.swift
+//  CovidList
+//
+//  Created by Sarah Lee on 6/19/22.
+//
+
+import SwiftUI
+
+@main
+struct CovidListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
